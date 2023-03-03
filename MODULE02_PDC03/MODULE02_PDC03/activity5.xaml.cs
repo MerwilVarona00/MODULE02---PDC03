0a0344be96b8d8d63a133827d1c656457c18f393
E14\User <galang.scottrylee@auf.edu.ph>
2023-03-03 10:14:30 +0800

    first commit

---
+++ MODULE02_PDC03/MODULE02_PDC03/activity5.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MODULE02_PDC03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity5 : ContentPage
+    {
+        ObservableCollection<employee> employees = new ObservableCollection<employee>();
+        public ObservableCollection<employee> Employees { get { return employees; } }
+
+        public activity5()
+        {
+            InitializeComponent();
+            {
+                lst.ItemsSource = employees;
+
+                employees.Add(new employee { DisplayName = "Juan Dela Cruz", Position = "post1.jpg" });
+                employees.Add(new employee { DisplayName = "Peter", Position = "post2.jpg" });
+                employees.Add(new employee { DisplayName = "Mary", Position = "post3.jpg" });
+
+            }
+
+        }
+    }
+}
---
+++ MODULE02_PDC03/MODULE02_PDC03/employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MODULE02_PDC03
+{
+    public class employee
+    {
+        public string DisplayName { get; set;}
+        public string Position { get; set; }
+        public string profileImage { get; set;} 
+    }
+}
